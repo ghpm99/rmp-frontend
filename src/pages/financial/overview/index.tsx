a43aca84bbbfbef3476c700d083d88035e57a111
Guilherme <ghpm99@hotmail.com>
2022-03-02 00:37:44 -0300

    Cria graficos adicionais

--- src/pages/financial/overview/index.tsx
+++ src/pages/financial/overview/index.tsx
@@ -1,36 +1,25 @@
 import { Breadcrumb, Layout } from 'antd'
 import { Content, Header } from 'antd/lib/layout/layout'
+import { BarElement, CategoryScale, Chart as ChartJS, Legend, LinearScale, LineElement, PointElement, Title, Tooltip } from 'chart.js'
+import { useEffect } from 'react'
+import { Bar, Line } from 'react-chartjs-2'
+import { useDispatch, useSelector } from 'react-redux'
 import LoadingPage from '../../../components/loadingPage/Index'
 import LoginHeader from '../../../components/loginHeader/Index'
 import MenuCollapsible from '../../../components/menu/Index'
-import styles from './Overview.module.css'
-import { Line } from 'react-chartjs-2'
-import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js'
-import { useDispatch, useSelector } from 'react-redux'
+import { fetchPaymentReport } from '../../../store/features/financial/Index'
 import { RootState } from '../../../store/store'
-import { useEffect } from 'react'
-import { fetchAllPayment, fetchPaymentReport } from '../../../store/features/financial/Index'
+import styles from './Overview.module.css'
 
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
+ChartJS.register(CategoryScale, LinearScale, BarElement, PointElement, LineElement, Title, Tooltip, Legend)
 
 function Overview() {
-  const financialStore = useSelector((state: RootState) => state.financial)
+  const financialStore = useSelector((state: RootState) => state.financial.paymentReport)
   const dispatch = useDispatch()
 
   useEffect(() => {
-    dispatch(fetchAllPayment({}))
     dispatch(fetchPaymentReport())
   }, [])
-
-  const findLabelName = (date) => {
-    const paymentDate = new Date(date)
-
-    const month = paymentDate.getUTCMonth() + 1
-
-    const labelName = `${month}/${paymentDate.getUTCFullYear()}`
-
-    return labelName
-  }
 
   const options = {
     responsive: true,
@@ -42,7 +31,7 @@ function Overview() {
     plugins: {
       title: {
         display: true,
-        text: 'Relatorio todos pagamentos abertos',
+        text: 'Relatorio pagamentos abertos sem mensalidades',
       },
     },
     scales: {
@@ -54,76 +43,131 @@ function Overview() {
     },
   }
 
-  const labels = []
-
-  financialStore.payments.data.forEach((payment) => {
-
-    const labelName = findLabelName(payment.payment_date)
-
-    const index = labels.indexOf(labelName)
-
-    if (index === -1) {
-      labels.push(labelName)
-    }
-  })
-
-  const dataCredit = labels.map(label => {
-    const creditArray = financialStore.payments.data
-      .filter(payment => payment.type === 0)
-      .filter(payment => label === findLabelName(payment.payment_date))
-      .map(payment => payment.value)
-
-    let valueCredit = 0
-
-    if (creditArray.length > 0) {
-      valueCredit = creditArray.reduce((previous, current) => parseFloat(previous) + parseFloat(current))
-    }
-
-    return valueCredit
-
-  })
-
-  const dataDebit = labels.map(label => {
-    const debitArray = financialStore.payments.data
-      .filter(payment => payment.type === 1)
-      .filter(payment => label === findLabelName(payment.payment_date))
-      .map(payment => payment.value)
-
-    let valueDebit = 0
-
-    if (debitArray.length > 0) {
-      valueDebit = debitArray.reduce((previous, current) => parseFloat(previous) + parseFloat(current))
-    }
-
-    return valueDebit
-
-  })
-
-  const dataDif = dataCredit.map((credit, index) => {
-    const valueDebit = dataDebit[index]
-    return credit - valueDebit
-  })
-
   const data = {
-    labels,
+    labels: financialStore.data.map(data => data.label),
     datasets: [
       {
         label: 'Credito',
-        data: dataCredit,
+        data: financialStore.data.map(data => data.credit),
         borderColor: 'rgb(53, 162, 235)',
         backgroundColor: 'rgba(53, 162, 235, 0.5)',
       },
       {
         label: 'Debito',
-        data: dataDebit,
+        data: financialStore.data.map(data => data.debit),
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      }
+    ],
+  }
+
+  const optionsFixed = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' as const,
+      },
+      title: {
+        display: true,
+        text: 'Relatorio pagamentos mensais',
+      },
+    },
+  }
+
+  const labels = ['Ativo']
+
+  const fixedCreditData = parseFloat(financialStore.data.map(data => {
+    if (data.fixed_credit) {
+      return parseFloat(data.fixed_credit)
+    } else {
+      return 0
+    }
+  }).reduce(((accum, curr) => accum + curr), 0).toFixed(2))
+
+  const fixedDebitData = parseFloat(financialStore.data.map(data => {
+    if (data.fixed_debit) {
+      return parseFloat(data.fixed_debit)
+    } else {
+      return 0
+    }
+  }).reduce(((accum, curr) => accum + curr), 0).toFixed(2))
+
+  const dataFixed = {
+    labels,
+    datasets: [
+      {
+        label: 'Credito',
+        data: [fixedCreditData],
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+      {
+        label: 'Debito',
+        data: [fixedDebitData],
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+    ],
+  }
+
+  const optionsAll = {
+    responsive: true,
+    interaction: {
+      mode: 'index' as const,
+      intersect: false,
+    },
+    stacked: false,
+    plugins: {
+      title: {
+        display: true,
+        text: 'Relatorio todos pagamentos com mensalidades',
+      },
+    },
+    scales: {
+      y: {
+        type: 'linear' as const,
+        display: true,
+        position: 'left' as const,
+      }
+    },
+  }
+
+  const valueCreditWithFixed = financialStore.data.map(data => {
+    if (data.credit) {
+      return parseFloat(data.credit) + fixedCreditData
+    } else {
+      return fixedCreditData
+    }
+  })
+
+  const valueDebitWithFixed = financialStore.data.map(data => {
+    if (data.debit) {
+      return parseFloat(data.debit) + fixedDebitData
+    } else {
+      return fixedDebitData
+    }
+  })
+
+  const valueDifWithFixed = valueCreditWithFixed.map((credit, index) => credit - valueDebitWithFixed[index] )
+
+  const dataAll = {
+    labels: financialStore.data.map(data => data.label),
+    datasets: [
+      {
+        label: 'Credito',
+        data: valueCreditWithFixed,
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+      {
+        label: 'Debito',
+        data: valueDebitWithFixed,
         borderColor: 'rgb(255, 99, 132)',
         backgroundColor: 'rgba(255, 99, 132, 0.5)',
       },
       {
         label: 'Diferença',
-        data: dataDif,
-        borderColor: 'rgb(99, 255, 138)',
-        backgroundColor: 'rgba(99, 255, 138, 0.5)',
+        data: valueDifWithFixed,
+        borderColor: 'rgb(99, 255, 151)',
+        backgroundColor: 'rgba(99, 255, 151, 0.5)',
       }
     ],
   }
@@ -142,7 +186,9 @@ function Overview() {
             <Breadcrumb.Item>Overview</Breadcrumb.Item>
           </Breadcrumb>
           <Layout>
+            <Line data={ dataAll } options={ optionsAll } width={ 400 } height={ 200 } />
             <Line data={ data } options={ options } width={ 400 } height={ 200 } />
+            <Bar data={ dataFixed } options={ optionsFixed } width={ 400 } height={ 200 } />
           </Layout>
         </Content>
       </Layout>
