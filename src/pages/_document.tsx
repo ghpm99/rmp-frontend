6902be1c419c33abbb484bd9b8e3477bc2a17276
Guilherme <ghpm99@hotmail.com>
2022-04-11 14:31:47 -0300

    Remove script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -23,29 +23,6 @@ class MyDocument extends Document {
 			<Html>
 				<Head>
 					<title>Remote Media Player</title>
-					<Script
-						strategy='afterInteractive'
-						dangerouslySetInnerHTML={ {
-							__html: `
-							(function(p,e,r,c,v,j,o){
-								p['PercAgenObject'] = v;p[v]=p[v]||function(){(p[v].q=p[v].q||[]).push(arguments)};p[v].t=1*new Date();
-								j=e.createElement(r);j.async=1;j.src=c;m=e.getElementsByTagName(r)[0];m.parentNode.insertBefore(j,m)
-								})(window, document, 'script', 'https://pcr.rec.br/pa.js', 'pa');
-								pa('create', '8d3c1ae5-f20d-4ab5-9f36-44e66c7cc968'); pa('page-view');
-
-								var urlParams = new URLSearchParams(window.location.search);
-								if (urlParams.get('pcrid')) {
-									var pa_data = {
-										"pcrid": urlParams.get('pcrid'),
-										"utm_source": urlParams.get('utm_source'),
-										"utm_medium": urlParams.get('utm_medium'),
-										"pcrtt": urlParams.get('pcrtt'),
-									};
-									pa('custom', 'affiliate-hook', {fields: pa_data});
-								}
-								`
-						} }
-					/>
 				</Head>
 				<body>
 					<Main />
